d76482f6f42f263a91e810880291a0dec297f9d5
Rafael Perez-Beato <rperezbeato@gmail.com>
2020-09-17 14:34:18 +0200

    Arreglados Imports de test.

--- src/test/java/com/rafa/conciliacion/presentation/controllers/BankingOperationControllerTest.java
+++ src/test/java/com/rafa/conciliacion/presentation/controllers/BankingOperationControllerTest.java
@@ -10,13 +10,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
